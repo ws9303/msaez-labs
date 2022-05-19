1f7690c804428ff04807b90934a1ff1b95227da8
Jinyoung Jang <jyjang@uengine.org>
2022-05-19 01:07:26 +0000

    f

---
+++ pubsub-compensation/delivery/src/main/java/shopmall/Delivery.java
@@ -0,0 +1,58 @@
+package shopmall;
+
+import javax.persistence.*;
+import org.springframework.beans.BeanUtils;
+import java.util.List;
+
+@Entity
+@Table(name="Delivery_table")
+public class Delivery {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private Long orderId;
+    private Long productId;
+    private String productName;
+
+    @PostPersist
+    public void onPostPersist(){
+        DeliveryStarted deliveryStarted = new DeliveryStarted();
+        BeanUtils.copyProperties(this, deliveryStarted);
+        deliveryStarted.publishAfterCommit();
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+
+
+
+}
---
+++ pubsub-compensation/delivery/src/main/java/shopmall/PolicyHandler.java
@@ -0,0 +1,30 @@
+package shopmall;
+
+import shopmall.config.kafka.KafkaProcessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PolicyHandler{
+    @Autowired
+    DeliveryRepository deliveryRepository;
+
+    @StreamListener(KafkaProcessor.INPUT)
+    public void wheneverOrderPlaced_StartDelivery(@Payload OrderPlaced orderPlaced){
+
+        if(orderPlaced.isMe()){
+            System.out.println("##### listener StartDelivery : " + orderPlaced.toJson());
+
+            Delivery delivery = new Delivery();
+            delivery.setOrderId(orderPlaced.getId());
+            delivery.setProductId(orderPlaced.getProductId());
+            delivery.setProductName(orderPlaced.getProductName());
+            deliveryRepository.save(delivery);
+        }
+    }
+
+}
